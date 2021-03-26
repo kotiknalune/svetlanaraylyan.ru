190b3f1c71b31e949d77fd71721150d2efa01bbf
Katya Railian <katerina.raylyan@gmail.com>
2021-03-26 19:11:45 +0300

    fix: responsive fixes

--- src/js/modules/burger.js
+++ src/js/modules/burger.js
@@ -3,17 +3,14 @@ const menu = document.querySelector('.menu');
 const burger = document.getElementById('burger');
 
 let opened = false;
-burger.addEventListener('click', () => {
-  if (!opened) {
-    lightbox.classList.add('lightbox-opened');
-    burger.classList.add('burger-opened');
-    menu.classList.add('menu-opened');
-    opened = !opened;
-  } else {
-    lightbox.classList.remove('lightbox-opened');
-    burger.classList.remove('burger-opened');
-    menu.classList.remove('menu-opened');
 
-    opened = !opened;
-  }
-});
+const toggleBurger = () => {
+  lightbox.classList.toggle('lightbox-opened');
+  burger.classList.toggle('burger-opened');
+  menu.classList.toggle('menu-opened');
+  opened = !opened;
+};
+
+burger.addEventListener('click', toggleBurger);
+
+// TODO: remove panel once link or not-panel is clicked
